ecfa3ababf26798294e9023abb44d9a093154bfe
yuzlaks <yuzyus268@gmail.com>
2020-08-10 10:52:15 +0700

    update form & fix

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -40,7 +40,7 @@
     </div>
     <div class="flex items-center justify-center h-screen">
 
-      <div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+      <div class="bg-white-800 text-black font-bold rounded-lg border shadow-lg p-10">
         Centered Content
       </div>
 
