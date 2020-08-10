7286dfad15df701b3fe45944490baa6a5effb423
yuzlaks <yuzyus268@gmail.com>
2020-08-10 08:57:22 +0700

    update form & fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="app" class="bg-gray">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/contact">Contact</router-link>
     </div>
+    -->
+    <nav class="bg-white px-8 pt-2 shadow-md">
+      <div class="-mb-px flex justify-center">
+        <a class="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8"
+          href="#">
+          Home
+        </a>
+        <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8"
+          href="#">
+          Products
+        </a>
+        <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8"
+          href="#">
+          Discounts
+        </a>
+        <a class="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3"
+          href="#">
+          Customers
+        </a>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
